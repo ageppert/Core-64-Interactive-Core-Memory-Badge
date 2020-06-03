0a230d292d2144418d10c2f51194760a01214c68
Andy Geppert <ageppert@freal.com>
2020-06-03 13:02:59 -0500

    Add note to consider FastSerial library

--- Firmware Source Code/Core-64-Interactive-Core-Memory-Badge/Serial_Debug.c
+++ Firmware Source Code/Core-64-Interactive-Core-Memory-Badge/Serial_Debug.c
@@ -9,6 +9,8 @@
 
 #define SERIAL_PORT_SPEED   15200
 
+// Consider using a <FastSerial.h> library?
+
 void SerialDebugSetup() {
   //Serial.begin(115200);
   // while (!Serial) { ; } // wait for serial port to connect. Needed for native USB port only
