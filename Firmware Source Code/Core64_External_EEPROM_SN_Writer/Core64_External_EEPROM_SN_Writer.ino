b48e529b210f51f23e8726889d8c6c03df7c86ba
Andy Geppert <ageppert@freal.com>
2021-01-09 11:25:45 -0600

    Expand hardware ID block to first four pages of external EEPROM Add XOR checksum for each pair of pages

--- Firmware Source Code/Core64_External_EEPROM_SN_Writer/Core64_External_EEPROM_SN_Writer.ino
+++ Firmware Source Code/Core64_External_EEPROM_SN_Writer/Core64_External_EEPROM_SN_Writer.ino
@@ -1,5 +1,12 @@
-/*
+/*  Core64 Hardware Identification - External 128 Byte EEPROM
+ *   Andy Geppert, January 9, 2021
+ *    First and second pages, bytes 0-31, provides unique ID (Serial Number) and baseline version info for each board
+ *    Third and fourth pages, bytes 32-63, are reserved for custom configuration data used when hacking Core64 hardware
+ *  User can use other pages as desired. 
+ *  MSB first, LSB last
+*/
 
+/*
   From https://www.hobbytronics.co.uk/arduino-external-eeprom
   Open output window at 115200bps
   For page write tutorial, adapt https://www.hobbytronics.co.uk/eeprom-page-write
@@ -15,15 +22,29 @@
   #define PAGE_SIZE_BYTES          16
   #define MAX_WRITE_TIME_MS         5
 #endif
-/*  Core64 Serial Number Identification
- *  Provides unique ID for each board so they can identify each other and work together through NFMI
- *  First page, 16 Bytes, of 128 Bytes external EEPROM is reserved for this data.
- *  User can use all other pages as desired.
- *  MSB first, LSB last
-                                     Serial Number (6)      |  Born on Date (3) |  Born as Version (3) | Checksum (1)
-*/
-const uint8_t string_len = 13;
-uint8_t StringToWrite[string_len] = {  0,  0,  0,  0,  0,  4,         21,  1,  1,             0,  4,  0,           0 };
+
+const uint8_t string_len = 64;
+const uint8_t owner_name_start = 14;
+const uint8_t owner_name_len = 17;
+const uint8_t page_one_two_checksum_position = 31;
+const uint8_t page_three_four_checksum_position = 63;
+uint8_t checksum = 0;
+uint8_t StringToWrite[string_len] = {                   // Start Byte # (# Bytes) Description
+  0,0,0,0,0,4,                                          // 0  ( 6) Serial Number  
+  21, 1, 1,                                             // 6  ( 3) Born on Date:  Year, Month, Day
+  0, 4, 0,                                              // 9  ( 3) Born as Version: Major.Minor.Revision
+  0,                                                    // 12 ( 1) Manufacturer ID
+  1,                                                    // 13 ( 1) Hardware ID Format Version
+  80,73,69,82,82,69,0,0, 0,0,0,0,0,0,0,0, 0,            // 14 (17) Owner name
+// P  I  E  R  R  E  
+  0,                                                    // 31 ( 1) Page 1&2 XOR Checksum (calculated below)
+
+  0,0,0,0,0,0,0,0,                                      // 32 ( 8) Hardware configuration reserved
+  0,0,0,0,0,0,0,0,                                      // 40 ( 8) reserved
+  0,0,0,0,0,0,0,0,                                      // 48 ( 8) reserved
+  0,0,0,0,0,0,0,                                        // 56 ( 7) reserved
+  0  };                                                 // 63 ( 1) Page 3&4 XOR Checksum (calculated below)
+ 
 
 void EEPROMExtWriteByte(int deviceaddress, unsigned int eeaddress, byte data ) 
 {
@@ -63,42 +84,51 @@ void setup(void)
   Serial.println("I2C EEPROM example");
   Serial.print("EEPROM PresentnPresent: ");
   Serial.println(I2CDetectExternalEEPROM(EEPROM_ADDRESS),DEC);
-  /*
-  unsigned int address = 0;
-  uint8_t myWrite = 44;
-  EEPROMExtWriteByte(EEPROM_ADDRESS, address, myWrite);
-  Serial.print("I write: ");
-  Serial.println(myWrite, DEC);
-
-  uint8_t myRead;
-  myRead = EEPROMExtReadByte(EEPROM_ADDRESS, address);
-  Serial.print("I read: ");
-  Serial.println(myRead, DEC);
-  */
 }
 
 void loop()
 {
   /*
-  // Write address locations 10-127 with 255-address, one byte at a time
-  for (uint8_t address=10; address < MEM_SIZE_BYTES; address++) {
-    uint8_t dataWrite = 0;
-    dataWrite = 255-address;
-    EEPROMExtWriteByte(EEPROM_ADDRESS, address, dataWrite);
-  }
+        Calculate checksums
+        Tested with: https://toolslick.com/math/bitwise/xor-calculator
   */
+  // Hard coded for bytes 0-30 with checksum stored in byte 31
+  checksum = StringToWrite[0];
+  for (uint8_t address = 0; address < 30; address++) {
+    checksum = checksum ^ StringToWrite[address+1];
+    StringToWrite[page_one_two_checksum_position] = checksum;
+  }
+  // Hard coded for bytes 32-62 with checksum stored in byte 63
+  checksum = StringToWrite[32];
+  for (uint8_t address = 32; address < 62; address++) {
+    checksum = checksum ^ StringToWrite[address+1];
+    StringToWrite[page_three_four_checksum_position] = checksum;
+  }
 
   /*
-        Write the serial number and hardware version
-   */
-  // TO DO: Calculate checksum
-  StringToWrite[string_len-1] = 222;
+        Write the data
+  */
   for (uint8_t address=0; address < string_len; address++) {
     uint8_t dataWrite = 0;
     dataWrite = StringToWrite[address];
     EEPROMExtWriteByte(EEPROM_ADDRESS, address, dataWrite);
   }
 
+  /*
+        Read the name and print it to the serial port
+  */
+  Serial.println();
+  Serial.print("Owner name: ");
+  for (uint8_t address=14; address < (owner_name_start + owner_name_len); address++) {
+    uint8_t dataRead = 0;
+    dataRead =  EEPROMExtReadByte(EEPROM_ADDRESS, address);
+    Serial.write(dataRead);
+  }
+  Serial.println();
+
+  /*
+        Read and print the data to the serial port
+  */
   for (uint8_t address=0; address < MEM_SIZE_BYTES; address++) {
     uint8_t dataRead = 0;
     dataRead =  EEPROMExtReadByte(EEPROM_ADDRESS, address);
@@ -106,5 +136,9 @@ void loop()
     Serial.print(" ");
     Serial.println(dataRead,DEC);
   }
+  
+  /*
+        End, infinite loop
+  */
   while(1);
 }
