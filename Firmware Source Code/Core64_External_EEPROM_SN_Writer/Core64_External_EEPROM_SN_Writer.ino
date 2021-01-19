4ced0d937e3e1d4a1b5c9ba1ee967200f4552fa6
Andy Geppert <ageppert@freal.com>
2021-01-18 22:11:08 -0600

    Add hardware config outline to external EEPROM writer

--- Firmware Source Code/Core64_External_EEPROM_SN_Writer/Core64_External_EEPROM_SN_Writer.ino
+++ Firmware Source Code/Core64_External_EEPROM_SN_Writer/Core64_External_EEPROM_SN_Writer.ino
@@ -44,7 +44,55 @@ uint8_t StringToWrite[string_len] = {                   // Start Byte # (# Bytes
   0,0,0,0,0,0,0,0,                                      // 48 ( 8) reserved
   0,0,0,0,0,0,0,                                        // 56 ( 7) reserved
   0  };                                                 // 63 ( 1) Page 3&4 XOR Checksum (calculated below)
- 
+
+  // TO DO: Implement the table below and expand the reserved space another 32 bytes.
+
+  /* Hardware Configuration use is intended to specify the as-shipped hardware configuration so the firmware can operate accordingly.
+  // Hardware Item #, Version #, both 8-bit fields. Example:
+      0, 1  LB EEPROM:      V1 M24C01, 128 Bytes
+                            V2 -
+      1, 1  POWER:          V1 Stock 4 "AAA" Generic Alkalines
+                            V2 4 "AAA" Long life lithium primary
+                            V3 3 "AA" generic
+                            V4 3 "AA" long life lithium primary
+                            V5 1S LiPo and mini charger
+      2, 1  CORE MEMORY:    V1 A single Standard 8x8
+                            V2 A full stack of 8 planes
+      3, 1  HALL SENSORS:   V1 SILABS I2C 0x30-0x33
+                            V2 Alternate
+      4, 1  AMBIENT LIGHT:  V1 LTR-329 at 0x29
+                            V2 Alternate
+      5, 1  SAO PORT X2:    V1 fully accessible I2C and 2 GPIO pins.
+                            V2 limited access, I2C only.
+      6, 1  CP1-8 PINS:     V1 1,2 used by SAO#2, 3-8 available.
+                            V2 1-8 used by 8 core planes.
+      7, 1  LED Array:      V1 Pimoroni Unicorn Hat 8x8 NeoPixel style. 
+                            V2 could be Unicorn hat HD.
+      8, 1  NEON PIXELS:    V1 SPI w/o CS, connected to SPI TFT LCD port
+                            V2 -
+      9, 1  LCD:            V1 color SPI 3.2"
+                            V2 color SPI ???
+     10, 1  OLED Display:   V1 monochrome I2C 128x64
+                            V2 color SPI
+                            V3 TeensyView 128x32
+     11, 1  SD CARD:        V1 Dedicated SD Card Expansion Header
+                            V2 connected to LCD Header
+                            V3 connected to OLED Header
+     12, 1  QWIIC PORT:     V1 open
+                            V2 -
+     13, 1  SAO PORT X1:    V1 fully accessible I2C and 2 GPIO pins.
+                            V2 limited access, I2C only.
+     14, 1  GPIO#1:         V1 Available generic use
+                            V2 Test config
+     15, 1  GPIO#2:         V1 Available generic use
+                            V2 Test config
+     16, 1  IR COMM:        V0 not populated
+                            V1 some basic config TBD
+     17, 1  NFMC:           V0 not populated
+                            V1 some basic config TBD
+     18, 1  RTC BATTERY:    V0 not populated
+                            V1 populated
+  */
 
 void EEPROMExtWriteByte(int deviceaddress, unsigned int eeaddress, byte data ) 
 {
